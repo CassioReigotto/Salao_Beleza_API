8b1cbb0b76aad620cb541e4b1b276a58b99931f3
Cassio Reigotto <cassio.reigotto@outlook.com>
2025-11-04 20:59:58 -0300

    fix : ajusta o tipo da coluna para receber inteiro.

--- src/main/java/com/example/salaobeleza/application/dto/request/ProfissionalRequestDTO.java
+++ src/main/java/com/example/salaobeleza/application/dto/request/ProfissionalRequestDTO.java
@@ -4,8 +4,6 @@ import com.example.salaobeleza.domain.model.enums.Especialidade;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-
-import java.time.DayOfWeek;
 import java.time.LocalTime;
 
 public record ProfissionalRequestDTO(
@@ -23,11 +21,12 @@ public record ProfissionalRequestDTO(
         Especialidade especialidade,
 
         @NotNull(message = "Dia de semana obrigatório")
-        DayOfWeek diaSemana,
+        Integer diaSemana,
 
         @NotNull(message = "Horário inicial obrigatório")
         LocalTime horarioInicio,
 
         @NotNull(message = "Horário final obrigatório")
         LocalTime horarioFim
+
 ) {}
