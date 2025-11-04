04593cad541a9a5d666276b5ea9782d3d7f32789
Cassio Reigotto <cassio.reigotto@outlook.com>
2025-11-04 20:53:24 -0300

    fix : ajustado um problema de receber null na data de disponibilidade

--- src/main/java/com/example/salaobeleza/infrastructure/mapper/ProfissionalMapper.java
+++ src/main/java/com/example/salaobeleza/infrastructure/mapper/ProfissionalMapper.java
@@ -1,12 +1,13 @@
 package com.example.salaobeleza.infrastructure.mapper;
 
+
 import com.example.salaobeleza.domain.model.Profissional;
 import com.example.salaobeleza.infrastructure.repository.entity.ProfissionalEntity;
-import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper (componentModel = "spring")
+
+@Mapper(componentModel = "spring")
 public interface ProfissionalMapper {
 
     @Mapping(source = "email.endereco" , target = "email")
@@ -14,10 +15,15 @@ public interface ProfissionalMapper {
     @Mapping(source = "disponibilidade.diaSemana", target = "diaSemana")
     @Mapping(source = "disponibilidade.horarioInicio", target = "horarioInicio")
     @Mapping(source = "disponibilidade.horarioFim", target = "horarioFim")
-     ProfissionalEntity ProfissionalDomainToProfissionalToEntity(Profissional profissional);
+    ProfissionalEntity ProfissionalDomainToProfissionalToEntity(Profissional profissional);
 
-    @InheritInverseConfiguration
-     Profissional ProfissionalEntityToProfissionalDomain(ProfissionalEntity profissionalEntity);
+    @Mapping(target = "email", expression = "java(new com.example.salaobeleza.domain.model.valueobject.Email(profissionalEntity.getEmail()))")
+    @Mapping(target = "disponibilidade", expression = "java(new com.example.salaobeleza.domain.model.valueobject.Disponibilidade(" +
+            "profissionalEntity.getDiaSemana(), " +
+            "profissionalEntity.getHorarioInicio(), " +
+            "profissionalEntity.getHorarioFim()))")
+    Profissional ProfissionalEntityToProfissionalDomain(ProfissionalEntity profissionalEntity);
 
 
 }
+
